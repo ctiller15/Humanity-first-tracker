95ebf5fd8ab959c584ab6ddedea355d695bb812a
Christopher <christiller2015@gmail.com>
2020-12-28 23:03:04 -0500

    Move various variables to env file. Update styles.

--- feed_reader/apps.py
+++ feed_reader/apps.py
@@ -1,6 +1,5 @@
 import os
 from django.apps import AppConfig
-from decouple import config
 from feed_reader.scheduler_override import Scheduler
 
 class FeedReaderConfig(AppConfig):
@@ -10,7 +9,6 @@ class FeedReaderConfig(AppConfig):
         if os.environ.get('RUN_MAIN', None) != 'true':
             from feed_reader.feed_job import job
             scheduler = Scheduler()
-            scheduler.every(15).seconds.do(job)
+            scheduler.every(15).minutes.do(job)
 
-            # do scheduler logic here.
             scheduler.run_continuously()
