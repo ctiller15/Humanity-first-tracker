4ec3198e41f2d9a0242280cccbbf4408571c7d54
Christopher <christiller2015@gmail.com>
2020-12-30 18:05:56 -0500

    Cutoff all articles not within the last 30 days.

--- articles/tests.py
+++ articles/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from feed_reader.models import Category, Entry
 from uuid import uuid4
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 def create_category(name):
@@ -10,8 +10,8 @@ def create_category(name):
     category.save()
     return category
 
-def create_article(category):
-    today_date = datetime.now().replace(tzinfo=pytz.UTC)
+def create_article(category, date=None):
+    today_date = date if date else datetime.now().replace(tzinfo=pytz.UTC)
     unique_identifier = uuid4()
     title = f'title{unique_identifier}'
     link = f'link{unique_identifier}'
@@ -45,3 +45,29 @@ class TestArticles(TestCase):
         response = self.client.get(url)
         
         self.assertEqual(response.status_code, 200)
+
+    def test_home_page_renders_last_thirty_days_of_results_by_default(self):
+        category_names = ['ubi', 'andrew_yang', 'automation']
+        articles = []
+        for category in category_names:
+            new_category = create_category(category)
+            new_article = create_article(new_category)
+            articles.append(new_article)
+
+        old_date = datetime.now(pytz.UTC) - timedelta(30)
+        old_articles = []
+        for category in category_names:
+            new_article = create_article(new_category, old_date)
+            old_articles.append(new_article)
+
+        response = self.client.get('/')
+        response_str = str(response.content, 'utf-8')
+
+        self.assertEqual(response.status_code, 200)
+        for article in articles:
+            self.assertIn(article.title, response_str)
+            self.assertIn(article.link, response_str)
+
+        for article in old_articles:
+            self.assertNotIn(article.title, response_str)
+            self.assertNotIn(article.link, response_str)
--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from feed_reader.models import Entry, Category
+from datetime import datetime, timedelta
+import pytz
+
+def get_entries(category_name):
+    today_date = datetime.now(pytz.utc)
+    cutoff_date = datetime.now(pytz.utc) - timedelta(days=30)
+    entries = Entry.objects.filter(category__name=category_name, hidden=False, updated__lte=today_date, updated__gt=cutoff_date).order_by('-updated')
+    return entries
 
 def home(request):
-    ubiEntries = Entry.objects.filter(category__name='ubi', hidden=False).order_by('-updated')
-    yangEntries = Entry.objects.filter(category__name='andrew_yang', hidden=False).order_by('-updated')
-    automationEntries = Entry.objects.filter(category__name='automation', hidden=False).order_by('-updated')
+    today_date = datetime.today()
+    cutoff_date = datetime.today() - timedelta(days=30)
+    ubiEntries = get_entries('ubi')
+    yangEntries = get_entries('andrew_yang')
+    automationEntries = get_entries('automation')
 
     yang_category, yang_category_created = Category.objects.get_or_create(name='andrew_yang')
     ubi_category, ubi_category_created = Category.objects.get_or_create(name='ubi')
